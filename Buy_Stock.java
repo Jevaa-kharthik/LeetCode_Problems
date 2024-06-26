cb0f0be9facad8f696d054cd7a2a5b2d62b5e581
Jevaa Kharthik N <113284998+Jevaa-kharthik@users.noreply.github.com>
2024-06-26 15:50:21 +0530

    121.Best Time to sell and buy stocks

---
+++ Buy_Stock.java
@@ -0,0 +1,20 @@
+public class Buy_Stock {
+    public static int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else if (prices[i] - minPrice > maxProfit) {
+                maxProfit = prices[i] - minPrice;
+            }
+        }
+
+        return maxProfit;
+    }
+    public static void main(String[] args) {
+        int arr[] = {7,6,4,3,1};
+        System.out.println(maxProfit(arr));
+    }
+}
--- Set_Matrix_Zero.java
+++ Set_Matrix_Zero.java
@@ -1,14 +1,23 @@
 public class Set_Matrix_Zero {
     public void setZeroes(int[][] matrix) {
-        for(int i = 0; i < matrix.length; i++){
+        Boolean[] row = new Boolean[matrix.length];
+        Boolean[] col = new Boolean[matrix[0].length];
+
+        for(int i=0; i < matrix.length; i++){
             for(int j = 0; j < matrix[i].length; j++){
                 if(matrix[i][j] == 0){
-                    matrix[i][j] = 0;
-                    matrix[j][i] = 0;
+                    row[i] = true;
+                    col[j] = true;
                 }
             }
         }
-
+        for(int i=0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[i].length; j++){
+                if(row[i] || col[j]){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
         
     }
     
