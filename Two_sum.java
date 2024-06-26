b9d1ef8c40ff66bac6a51581679161fb1a7c8e0d
Jevaa Kharthik N <113284998+Jevaa-kharthik@users.noreply.github.com>
2024-06-26 21:41:21 +0530

    1. Two sum

---
+++ Two_sum.java
@@ -0,0 +1,10 @@
+public class Two_sum {
+    public int[] twoSum(int[] nums, int target) {
+        for(int i=1;i<nums.length;i++)
+            for(int j=i;j<nums.length;j++)
+                if(nums[j]+nums[j-i]==target){
+                    return new int[]{j-i,j};
+                }
+      return null;
+    }
+}
