a16bd415c840420bb2a6d8271fd0cc4145bbd3c2
Jevaa Kharthik N <113284998+Jevaa-kharthik@users.noreply.github.com>
2024-06-26 21:55:52 +0530

    122. Best time to buy and sell stock II

---
+++ Buy_and_sell_stock.java
@@ -0,0 +1,12 @@
+public class Buy_and_sell_stock {
+    public int maxProfit(int[] prices) {
+        int max = 0;
+        for(int i = 1; i < prices.length; i++){
+            if(prices[i] > prices[i - 1]){
+                max += prices[i] - prices[i-1];
+            }
+        }
+        return max;
+    }
+    
+}
