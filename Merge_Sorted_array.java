c1d2e77ec88a3b318e0e6f0c8cf3ca1f614a70d3
Jevaa Kharthik N <113284998+Jevaa-kharthik@users.noreply.github.com>
2024-06-30 23:39:58 +0530

    88. Merge Sorted Array

---
+++ Merge_Sorted_array.java
@@ -0,0 +1,22 @@
+
+public class Merge_Sorted_array {
+    class Solution {
+        public void merge(int[] nums1, int m, int[] nums2, int n) {
+            int p1 = m - 1;
+            int p2 = n - 1;
+            int point = m + n -1;
+
+            while(p1 >= 0 && p2 >= 0){
+                if (nums1[p1] > nums2[p2]) {
+                    nums1[point--] = nums1[p1--];
+                } else {
+                    nums1[point--] = nums2[p2--];
+                }
+            }
+
+            while(p2 >= 0){
+                nums1[point--] = nums2[p2--];
+            }
+        }
+    }
+}
