d4f995144c36980a4213492ba429e527d7d5a486
Jevaa Kharthik N <113284998+Jevaa-kharthik@users.noreply.github.com>
2024-07-02 19:18:16 +0530

    Game Of Life

---
+++ Game_of_Life.java
@@ -0,0 +1,18 @@
+public class Game_of_Life {
+    public void gameOfLife(int[][] board) {
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[i].length; j++){
+                @SuppressWarnings("unused")
+                int zero_count = 0;
+                @SuppressWarnings("unused")
+                int ones_count = 0;
+                if(board[i][j] == 0){
+                    
+                }
+                else if(board[i][j] == 1){
+
+                }
+            }
+        }
+    }
+}
